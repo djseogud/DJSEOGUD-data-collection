5af0c7b1c5dbe3f364249ac62c2e251db1e06c93
kanghyun <kanghyun@ad.unc.edu>
2017-07-25 13:07:13 -0400

    changed data-collection.sh, write-to-db.sh

--- data-collection.sh
+++ data-collection.sh
@@ -3,19 +3,41 @@
 echo "What is your last name?"
 read LASTNAME
 
-echo "In what year were you born?"
-read BORNYEAR
+echo "How old are you?"
+read AGE
 
 echo "Which country, that you haven't visited, do you want to visit the most"
-read VISITCOUNTRY
+read COUNTRY
 
-echo ""
+echo "What is your favorite ice cream flavor?"
+read ICECREAM
 
-echo ""
+echo "How many siblings do you have?"
+read SIBLING
+
 TIMESTAMP=`date --iso-8601=seconds`
 
-IDENTIFIER = "`echo '$RANDOM$RANDOM$RANDOM' | sha256sum | sed 's/[\t\s-]//g'`"
-echo "$"
+IDENTIFIER="`echo $RANDOM$RANDOM$RANDOM | sha256sum | sed 's/[^0-9a-fA-F]//g' | sed -e 's/^/0x/'`"
+
+# Write data to tmp.csv
+
+echo "$IDENTIFIER,$TIMESTAMP,$LASTNAME,$AGE,$COUNTRY,$ICECREAM,$SIBLING" > ./tmp.csv
+
+# Read out the data in the CSV file
+
+cat ./tmp.csv
+
+# Write data to database
+
+bash ./$GHUSERNAME-write-to-db.sh
+
+# Back up data
+
+cat ./tmp.csv >> data-backup.csv
+
+# Remove temp file
+
+rm tmp.csv
 
 ...>> ./tmp.csv
 
